a6f9b183b54ec5506b25c09a759d20f01d30e80e
Blaise <menniablaise@yahoo.com>
2026-03-21 22:38:47 +0000

    feat: add school registration page and update application routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Inbox from './pages/Inbox';
+import RegisterSchool from './pages/RegisterSchool';
 
 function App() {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/inbox" element={<Inbox />} />
-        </Routes>
-      </MainLayout>
+      <Routes>
+        <Route path="/register-school" element={<RegisterSchool />} />
+        
+        <Route path="/*" element={
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/inbox" element={<Inbox />} />
+            </Routes>
+          </MainLayout>
+        } />
+      </Routes>
     </Router>
   );
 }
